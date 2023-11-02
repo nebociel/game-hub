d12d04adf76f86e24350a698e017e1243bfc8ac5
nebociel <akashchaubey443@gmail.com>
2023-11-02 23:08:37 +0530

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// ccfgwofcwbyprdwbhh@ckptr.com
+// ccfgwo
+export default axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: 'bca5a352cc5a4cd9b6beb936fdef51a6',
+  },
+});
