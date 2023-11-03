4a2eda38ac72dad3e8f6ee6d6216956325e4817a
nebociel <akashchaubey443@gmail.com>
2023-11-03 18:44:05 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,10 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url)
-    return 'https://placehold.co/600x400/000000/FFFFFF.webp?text=No+Image';
+  // if (!url)
+  //   return 'https://placehold.co/600x400/000000/FFFFFF.webp?text=No+Image';
+
+  if (!url) return noImage;
 
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
