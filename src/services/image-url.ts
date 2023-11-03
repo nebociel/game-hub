bffd8092a9f3d27fa5b552e47db51286c66ee025
nebociel <akashchaubey443@gmail.com>
2023-11-03 18:41:20 +0530

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,7 @@
 const getCroppedImageUrl = (url: string) => {
+  if (!url)
+    return 'https://placehold.co/600x400/000000/FFFFFF.webp?text=No+Image';
+
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
