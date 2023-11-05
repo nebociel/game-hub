12e50e5fbbe24a5c1266c7a6cc691b4a83668b29
nebociel <akashchaubey443@gmail.com>
2023-11-05 21:55:58 +0530

    Refactoring - create a reusable API client

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import apiClient, { FetchResponse } from '../services/api-client';
+import APIClient from '../services/api-client';
 import genres from '../data/genres';
+
+const apiClient = new APIClient<Genre>('/genres');
 
 export interface Genre {
   id: number;
@@ -11,13 +13,21 @@ export interface Genre {
 // const useGenres = () => useData<Genre>('/genres');
 // const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
+// const useGenres = () =>
+//   useQuery({
+//     queryKey: ['genres'],
+//     queryFn: () =>
+//       apiClient
+//         .get<FetchResponse<Genre>>('/genres')
+//         .then((response) => response.data),
+//     staleTime: 24 * 60 * 60 * 1000, // 1 day
+//     initialData: { count: genres.length, results: genres },
+//   });
+
 const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Genre>>('/genres')
-        .then((response) => response.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 1 day
     initialData: { count: genres.length, results: genres },
   });
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
@@ -7,9 +7,25 @@ export interface FetchResponse<T> {
 
 // ccfgwofcwbyprdwbhh@ckptr.com
 // ccfgwo
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: 'bca5a352cc5a4cd9b6beb936fdef51a6',
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((response) => response.data);
+  };
+}
+
+export default APIClient;
