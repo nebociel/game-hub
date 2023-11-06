18a756ee6378c3aa24f96280b703c3844beffcfc
nebociel <akashchaubey443@gmail.com>
2023-11-06 09:57:47 +0530

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
 import APIClient, { FetchResponse } from '../services/api-client';
 import { Platform } from './usePlatforms';
@@ -45,18 +45,37 @@ export interface Game {
 //     staleTime: 24 * 60 * 60 * 1000, // 1 day
 //   });
 
+// const useGames = (gameQuery: GameQuery) =>
+//   useQuery<FetchResponse<Game>, Error>({
+//     queryKey: ['games', gameQuery],
+//     queryFn: () =>
+//       apiClient.getAll({
+//         params: {
+//           genres: gameQuery.genre?.id,
+//           parent_platforms: gameQuery.platform?.id,
+//           ordering: gameQuery.sortOrder,
+//           search: gameQuery.searchText,
+//         },
+//       }),
+//     staleTime: 24 * 60 * 60 * 1000, // 1 day
+//   });
+
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
     staleTime: 24 * 60 * 60 * 1000, // 1 day
   });
 
