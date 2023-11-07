ac5c0848de83cd080be0ed317b864af0ea2c311f
nebociel <akashchaubey443@gmail.com>
2023-11-07 17:56:38 +0530

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,25 +1,18 @@
-import { Card, CardBody, Image, Heading, HStack } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import { Game } from '../hooks/useGames';
-import PlatformIconList from './PlatformIconList';
-import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import CriticScore from './CriticScore';
 import Emoji from './Emoji';
-import { useNavigate } from 'react-router-dom';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  const navigate = useNavigate();
-
   return (
-    <Card
-      onClick={(event) => {
-        event.preventDefault();
-        navigate(`/game/${game.id}`);
-      }}
-    >
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         {/* {game.parent_platforms.map(({ platform }) => (
@@ -32,7 +25,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize='2xl'>
-          {game.name}
+          <Link to={`/games/${game.slug}`}>{game.name}</Link>
           <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -1,9 +1,25 @@
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/useGame';
+import { Heading, Spinner, Text } from '@chakra-ui/react';
 
-
-const GameDetailPage = () => {
-  return (
-    <div>GameDetailPage</div>
-  )
+function GameDescription({ game }) {
+  return <div dangerouslySetInnerHTML={{ __html: game.description }} />;
 }
 
-export default GameDetailPage
+const GameDetailPage = () => {
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+      {/* <GameDescription game={game} /> */}
+    </>
+  );
+};
+
+export default GameDetailPage;
